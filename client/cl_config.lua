0f385a28b6e7271bb7700b69557beaa00d31ac3f
pratinha10 <pratinha_joao@hotmail.com>
2022-08-25 04:00:14 +0100

    Update cl_config.lua
    Criação das orgs:
    lostmc
    ballas
    vagos
    cartel
    families (Los Zetas)
    triads (18th street Gang)
    camorra
    peakyblinders
    aztecas
    bloods
    piratas
    crips
    outlaws
    fazendeiros
    grove
    mafia

--- client/cl_config.lua
+++ client/cl_config.lua
@@ -47,19 +47,52 @@ Config.BossMenuZones = {
 
 Config.GangMenus = {
     ['lostmc'] = {
-        vector3(0, 0, 0),
+        vector3(150.64, 329.31, 111.89),
     },
     ['ballas'] = {
-        vector3(0,0, 0),
+        vector3(108.47, -1981.87, 20.96),
     },
     ['vagos'] = {
-        vector3(0, 0, 0),
+        vector3(332.36, -2013.06, 22.39),
     },
     ['cartel'] = {
-        vector3(0, 0, 0),
+        vector3(-1791.57, 438.19, 128.51),
     },
     ['families'] = {
-        vector3(0, 0, 0),
+        vector3(-804.68, 177.32, 72.83),
+    },
+    ['triads'] = {
+        vector3(1439.1, -1489.22, 66.62),
+    },
+    ['camorra'] = {
+        vector3(-1512.71, 840.77, 186.14),
+    },
+    ['peakyblinders'] = {
+        vector3(-2581.26, 1892.02, 163.72),
+    },
+    ['aztecas'] = {
+        vector3(-1092.9, -1669.45, 8.41),
+    },
+    ['bloods'] = {
+        vector3(-1568.53, -405.48, 48.26),
+    },
+    ['piratas'] = {
+        vector3(3147.46, 2215.32, 3.19),
+    },
+    ['crips'] = {
+        vector3(-1572.12, -234.48, 54.82),
+    },
+    ['outlaws'] = {
+        vector3(-1572.12, -234.48, 54.82),
+    },
+    ['fazendeiros'] = {
+        vector3(2143.12, 3901.51, 34.37),
+    },
+    ['grove'] = {
+        vector3(-137.0, -1608.75, 35.03),
+    },
+    ['mafia'] = {
+        vector3(-1525.82, 148.72, 60.79),
     },
 }
 
@@ -69,4 +102,52 @@ Config.GangMenuZones = {
         { coords = vector3(0.0, 0.0, 0.0), length = 0.0, width = 0.0, heading = 0.0, minZ = 0.0, maxZ = 0.0 },
     },
     ]]
+    ['lostmc'] = {
+        { coords = vector3(150.64, 329.31, 111.89), length = 0.35, width = 0.45, heading = 206.26, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['ballas'] = {
+        { coords = vector3(108.47, -1981.87, 20.96), length = 0.35, width = 0.45, heading = 11.95, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['vagos'] = {
+        { coords = vector3(332.36, -2013.06, 22.39), length = 0.35, width = 0.45, heading = 50.38, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['cartel'] = {
+        { coords = vector3(-1791.57, 438.19, 128.51), length = 0.35, width = 0.45, heading = 0.37, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['families'] = {
+        { coords = vector3(-804.68, 177.32, 72.83), length = 0.35, width = 0.45, heading = 14.26, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['triads'] = {
+        { coords = vector3(1439.1, -1489.22, 66.62), length = 0.35, width = 0.45, heading = 206.26, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['camorra'] = {
+        { coords = vector3(-1512.71, 840.77, 186.14), length = 0.35, width = 0.45, heading = 254.71, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['peakyblinders'] = {
+        { coords = vector3(-2581.26, 1892.02, 163.72), length = 0.35, width = 0.45, heading = 307.25, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['aztecas'] = {
+        { coords = vector3(-1092.9, -1669.45, 8.41), length = 0.35, width = 0.45, heading = 206.26, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['bloods'] = {
+        { coords = vector3(-1568.53, -405.48, 48.26), length = 0.35, width = 0.45, heading = 47.61, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['piratas'] = {
+        { coords = vector3(3147.46, 2215.32, 3.19), length = 0.35, width = 0.45, heading = 217.29, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['crips'] = {
+        { coords = vector3(-1572.12, -234.48, 54.82), length = 0.35, width = 0.45, heading = 246.91, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['outlaws'] = {
+        { coords = vector3(-368.68, 75.7, 54.42), length = 0.35, width = 0.45, heading = 87.4, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['fazendeiros'] = {
+        { coords = vector3(2143.12, 3901.51, 34.37), length = 0.35, width = 0.45, heading = 275.93, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['grove'] = {
+        { coords = vector3(-137.0, -1608.75, 35.03), length = 0.35, width = 0.45, heading = 70.15, minZ = 30.58, maxZ = 30.68 } ,
+    },
+    ['mafia'] = {
+        { coords = vector3(-1525.82, 148.72, 60.79), length = 0.35, width = 0.45, heading = 271.27, minZ = 30.58, maxZ = 30.68 } ,
+    },
 }
